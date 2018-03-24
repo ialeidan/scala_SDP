bf6c4d4aec31bc4c24a4c7545f47929442065766
iJawad <ijawaddev@gmail.com>
2018-03-24 19:39:14 +0300

    DB

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -48,39 +48,39 @@ object Application extends Controller {
       val name = (jsValue \ "name")
       val password = (jsValue \ "password")
 
-//      Ok(Json.toJson(Map("status" -> "OK", "message" -> ("Hello " + name.toString))))
-      Ok(Play.configuration.getString("mongodb.uri").get)
+      Ok(Json.toJson(Map("status" -> "OK", "message" -> ("Hello " + name.toString))))
+//      Ok(Play.configuration.getString("mongodb.uri").get)
     }.getOrElse{
       BadRequest(Json.toJson(Map("status" -> "KO", "message" -> "Missing parameter [name]")))
     }
   }
 
 
-//  def testDB = Action { implicit request =>
-//    var out = request.body.asJson
-//    var ret = "";
-//
-//
-//    val coll = mongDB("Users")
-//
-//    val allDocs = coll.find()
-//    println( allDocs )
-//    for(doc <- allDocs){
-//        ret += " / " + doc.toString()
-//    }
-//
-//    out.map {jsValue =>
-//      val name = (jsValue \ "name")
-//      val password = (jsValue \ "password")
-//
-//
-//
-//
-////      Ok(Json.toJson(Map("status" -> "OK", "message" -> ("Hello " + name.toString))))
-//      Ok(ret)
-//    }.getOrElse{
-//      BadRequest(Json.toJson(Map("status" -> "KO", "message" -> "Missing parameter [name]")))
-//    }
-//  }
+  def testDB = Action { implicit request =>
+    var out = request.body.asJson
+    var ret = "";
+
+
+    val coll = mongDB("Users")
+
+    val allDocs = coll.find()
+    println( allDocs )
+    for(doc <- allDocs){
+        ret += " / " + doc.toString()
+    }
+
+    out.map {jsValue =>
+      val name = (jsValue \ "name")
+      val password = (jsValue \ "password")
+
+
+
+
+//      Ok(Json.toJson(Map("status" -> "OK", "message" -> ("Hello " + name.toString))))
+      Ok(ret)
+    }.getOrElse{
+      BadRequest(Json.toJson(Map("status" -> "KO", "message" -> "Missing parameter [name]")))
+    }
+  }
 
 }
