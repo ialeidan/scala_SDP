ec50662a844a4d16d7a7f8902493bf139ddc31e7
iJawad <ijawaddev@gmail.com>
2018-03-24 19:47:44 +0300

    DB

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,26 +20,6 @@ object Application extends Controller {
     Ok(views.html.index(null))
   }
 
-  def db = Action {
-    var out = ""
-    val conn = DB.getConnection()
-    try {
-      val stmt = conn.createStatement
-
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
-
-      val rs = stmt.executeQuery("SELECT tick FROM ticks")
-
-      while (rs.next) {
-        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
-      }
-    } finally {
-      conn.close()
-    }
-    Ok(out)
-  }
-
 
   def test = Action { implicit request =>
     var out = request.body.asJson
