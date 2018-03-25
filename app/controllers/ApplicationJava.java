002fead5337da9d094cdb9c8f4b665d5303b8713
Mohammed <jeezany@gmail.com>
2018-03-25 17:03:38 +0300

    Message

--- app/controllers/ApplicationJava.java
+++ app/controllers/ApplicationJava.java
@@ -14,21 +14,21 @@ public class ApplicationJava extends Controller{
     private DatabaseJava database = new DatabaseJava();
 
     public Result index1() {
-        JsonNode Id =request().body().asJson();
-
-
-        HashMap<String, Object> ret = new HashMap<String, Object>(){
-            {
-                put("id", Id.get("id").intValue()+1);
-                put("username", Id.get("username"));
-            }
-        };
+//        JsonNode Id =request().body().asJson();
+//
+//
+//        HashMap<String, Object> ret = new HashMap<String, Object>(){
+//            {
+//                put("id", Id.get("id").intValue()+1);
+//                put("username", Id.get("username"));
+//            }
+//        };
 
         database.Add();
         database.testRead();
 
 
-        return ok(Json.toJson(ret));
+        return ok("jhhi");
 //        return ok(Id);
 //        return ok(Id.get("id").intValue() + "");
 
